d2324a249cd9a284f65c9072a7504fde0f06b07e
Md Asikuzzaman <mdasikuzzaman.en@gmail.com>
2026-08-31 16:50:02 +0600

    feat: added navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,14 @@
-import React from "react";
+import Container from "./Container";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <header className="">
+      <Container className="py-3">
+        <h1 className="heading-page">Tasks</h1>
+        <p className="paragraph-lg">Manage and track your team's work</p>
+      </Container>
+    </header>
+  );
 };
 
 export default Navbar;
